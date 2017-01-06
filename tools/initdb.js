88af9e370c55aab2712d04ffb8f2edf9dd7935e9
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2017-01-06 20:36:07 +0200

    Update dependencies

--- tools/initdb.js
+++ tools/initdb.js
@@ -7,12 +7,18 @@ reminders.remove().value();
 reminders.insert({
   title: 'Feed Tiger',
   body: `Don't forget to feed the cat.`,
-  when: moment().add(8, 'days').add(2, 'hours').toDate(),
+  when: moment().add(2, 'hours').toDate(),
   createdAt: new Date
 }).value();
 reminders.insert({
-  title: 'Buy groceries',
-  body: '...and whatever else is necessary...',
+  title: 'Buy a new phone',
+  body: 'Go get a new phone, since the old one could explode at any time...',
   when: moment().add(4, 'days').add(12, 'hours').toDate(),
+  createdAt: new Date
+}).value();
+reminders.insert({
+  title: 'Pay rent',
+  body: `Next week, don't foreget to pay the rent.`,
+  when: moment().add(5, 'days').add(3, 'hours').toDate(),
   createdAt: new Date
 }).value();
