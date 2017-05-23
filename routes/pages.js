f97b51a88ebc9930d768362c22733d715240ff19
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2017-05-23 23:35:21 +0300

    Update lots of deps, fix #3

--- routes/pages.js
+++ routes/pages.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('home', { title: 'Quickstart' });
 });
 
-router.get('/app', (req, res, next) => {
+router.get('/app', (req, res) => {
   res.render('app');
 });
 
